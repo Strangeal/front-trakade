51b0bb07eeaa65f9d20b870d53dad987d6c7fe6c
strangeal <bajerijustice@gmail.com>
2023-04-20 13:25:21 +0000

    Add register and signin slice

---
+++ src/redux/authen/authSlice.ts
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { UserProps } from "../../Types";
+import { createNewUser, singInUser } from "../api/endpoint";
+
+const initialState = {
+  status: "",
+};
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (user: UserProps, ThunkApi) => {
+    try {
+      const response = await createNewUser(user);
+      return response.data;
+    } catch (err: any) {
+      return ThunkApi.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (user: UserProps, ThunkApi) => {
+    try {
+      const response = await singInUser(user);
+      return response.data;
+    } catch (err: any) {
+      return ThunkApi.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        const isPending = state;
+        isPending.status = "pending";
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        const isFulfilled = state;
+        isFulfilled.status = "fulfilled";
+      })
+      .addCase(registerUser.rejected, (state) => {
+        const isRejected = state;
+        isRejected.status = "rejected";
+      })
+      .addCase(loginUser.pending, (state) => {
+        const isPending = state;
+        isPending.status = "pending";
+      })
+      .addCase(loginUser.fulfilled, (state) => {
+        const isFulfilled = state;
+        isFulfilled.status = "fulfilled";
+      })
+      .addCase(loginUser.rejected, (state) => {
+        const isRejected = state;
+        isRejected.status = "rejected";
+      });
+  },
+});
+
+export default authSlice.reducer;
