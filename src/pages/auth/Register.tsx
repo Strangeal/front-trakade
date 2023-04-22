f1c2fc3f9a0cc44bcc7c3c6993a14db1ac62ce76
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-22 13:03:18 +0000

    Handle client authentication

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -4,6 +4,9 @@ import { ErrorMessage, Field, FieldProps, Form, Formik } from 'formik';
 import { MdEmail, MdPassword } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
+import handleToastr from '../../helpers/handleToastr';
+import { useAppDispatch } from '../../helpers/useRedux';
+import { registerUser } from '../../redux/authen/authSlice';
 import styles from '../../styles/Register.module.css';
 
 type RegisterUserProps = {
@@ -20,7 +23,6 @@ const initialValues: RegisterUserProps = {
 };
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required('Full name is required'),
   email: Yup.string().email('Invalid email').required('Email is required'),
   password: Yup.string()
     .min(6, 'Password must be at least 6 characters')
@@ -31,9 +33,30 @@ const validationSchema = Yup.object().shape({
 });
 
 const Register = () => {
+  const dispatch = useAppDispatch();
+
   const onSubmit = (values: RegisterUserProps) => {
-    console.log(values);
+    const user = {
+      email: values.email,
+      password: values.password,
+    };
+
+    dispatch(registerUser(user)).then((res: any) => {
+      console.log(res);
+      if (res.meta.requestStatus === 'fulfilled') {
+        const message = 'Signed up successfully😊';
+        const icon = 'success';
+        handleToastr(message, icon);
+      }
+      if (res.meta.requestStatus === 'rejected') {
+        const message = 'Sign up unsuccessful🥺';
+        const text = res.payload ? res.payload.error_description[0] : '';
+        const icon = 'error';
+        handleToastr(message, icon, text);
+      }
+    });
   };
+
   return (
     <section className={styles.register}>
       <div className={styles['register-banner']}>
@@ -44,7 +67,7 @@ const Register = () => {
           <img
             src="https://res.cloudinary.com/dskl0qde4/image/upload/v1682045366/Mobile-login_cropped_via_10015_io_bw8prl.png"
             alt=""
-            className='w-[18rem] m-auto'
+            className="w-[18rem] m-auto"
           />
         </div>
       </div>
@@ -57,97 +80,94 @@ const Register = () => {
           validationSchema={validationSchema}
           onSubmit={onSubmit}
         >
-          {(formik) => {
-            return (
-              <Form>
-                <Field name="email">
-                  {({ field, meta }: FieldProps) => {
-                    return (
-                      <div className="mb-5">
-                        <TextField
-                          id="name"
-                          type="email"
-                          fullWidth
-                          label="Email"
-                          color="success"
-                          variant="outlined"
-                          required
-                          {...field}
-                          error={meta.touched && meta.error ? true : false}
-                          helperText={<ErrorMessage name="email" />}
-                          InputProps={{
-                            startAdornment: (
-                              <InputAdornment position="start">
-                                <MdEmail />
-                              </InputAdornment>
-                            ),
-                          }}
-                        />
-                      </div>
-                    );
-                  }}
-                </Field>
-                <Field name="password">
-                  {({ field, meta }: FieldProps) => {
-                    return (
-                      <div className="mb-5">
-                        <TextField
-                          id="password"
-                          type="password"
-                          fullWidth
-                          label="Password"
-                          color="success"
-                          variant="outlined"
-                          required
-                          {...field}
-                          error={meta.touched && meta.error ? true : false}
-                          helperText={<ErrorMessage name="password" />}
-                          InputProps={{
-                            startAdornment: (
-                              <InputAdornment position="start">
-                                <MdPassword />
-                              </InputAdornment>
-                            ),
-                          }}
-                        />
-                      </div>
-                    );
-                  }}
-                </Field>
-                <Field name="confirm_password">
-                  {({ field, meta }: FieldProps) => {
-                    return (
-                      <div className="mb-5">
-                        <TextField
-                          id="confirm_password"
-                          type="password"
-                          fullWidth
-                          label="Confirm Password"
-                          color="success"
-                          variant="outlined"
-                          required
-                          {...field}
-                          error={meta.touched && meta.error ? true : false}
-                          helperText={<ErrorMessage name="confirm_password" />}
-                          InputProps={{
-                            startAdornment: (
-                              <InputAdornment position="start">
-                                <MdPassword />
-                              </InputAdornment>
-                            ),
-                          }}
-                        />
-                      </div>
-                    );
-                  }}
-                </Field>
-                <Button variant="contained" fullWidth color="success">
-                  Register
-                </Button>
-              </Form>
-            );
-          }}
+          <Form>
+            <Field name="email">
+              {({ field, meta }: FieldProps) => {
+                return (
+                  <div className="mb-5">
+                    <TextField
+                      id="email"
+                      type="email"
+                      fullWidth
+                      label="Email"
+                      color="success"
+                      variant="outlined"
+                      required
+                      {...field}
+                      error={meta.touched && meta.error ? true : false}
+                      helperText={<ErrorMessage name="email" />}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <MdEmail />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </div>
+                );
+              }}
+            </Field>
+            <Field name="password">
+              {({ field, meta }: FieldProps) => {
+                return (
+                  <div className="mb-5">
+                    <TextField
+                      id="password"
+                      type="password"
+                      fullWidth
+                      label="Password"
+                      color="success"
+                      variant="outlined"
+                      required
+                      {...field}
+                      error={meta.touched && meta.error ? true : false}
+                      helperText={<ErrorMessage name="password" />}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <MdPassword />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </div>
+                );
+              }}
+            </Field>
+            <Field name="confirm_password">
+              {({ field, meta }: FieldProps) => {
+                return (
+                  <div className="mb-5">
+                    <TextField
+                      id="confirm_password"
+                      type="password"
+                      fullWidth
+                      label="Confirm Password"
+                      color="success"
+                      variant="outlined"
+                      required
+                      {...field}
+                      error={meta.touched && meta.error ? true : false}
+                      helperText={<ErrorMessage name="confirm_password" />}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <MdPassword />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </div>
+                );
+              }}
+            </Field>
+            <Button type="submit" variant="contained" fullWidth color="success">
+              Register
+            </Button>
+          </Form>
         </Formik>
+
         <div className="text-center text-sm">
           <p className="my-3">
             By signing up, I agree to Trakade{' '}
