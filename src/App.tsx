52f3810cedfcf2078060219086b7cab3d70d2fb8
strangeal <bajerijustice@gmail.com>
2023-04-24 11:04:14 +0000

    Import themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./components/Theme";
 
 function App() {
   return (
     <div className="App">
-      <Outlet />
+      <ThemeProvider theme={theme}>
+        <Outlet />
+      </ThemeProvider>
     </div>
   );
 }
