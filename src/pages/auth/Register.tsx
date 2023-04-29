74348dc41e8dbfefddce85bcdba8ddf3b13bbc04
strangeal <bajerijustice@gmail.com>
2023-04-29 12:41:57 +0000

    Add render messages in register

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,13 +1,13 @@
-import { Button, InputAdornment } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import { ErrorMessage, Field, FieldProps, Form, Formik } from 'formik';
-import { MdEmail, MdPassword } from 'react-icons/md';
-import { Link } from 'react-router-dom';
-import * as Yup from 'yup';
-import handleToastr from '../../helpers/handleToastr';
-import { useAppDispatch } from '../../helpers/useRedux';
-import { registerUser } from '../../redux/authen/authSlice';
-import styles from '../../styles/Register.module.css';
+import { Button, InputAdornment } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import { ErrorMessage, Field, FieldProps, Form, Formik } from "formik";
+import { MdEmail, MdPassword } from "react-icons/md";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
+import handleToastr from "../../helpers/handleToastr";
+import { useAppDispatch } from "../../helpers/useRedux";
+import { registerUser } from "../../redux/authen/authSlice";
+import styles from "../../styles/Register.module.css";
 
 type RegisterUserProps = {
   email: string;
@@ -17,19 +17,19 @@ type RegisterUserProps = {
 
 const initialValues: RegisterUserProps = {
   // name: '',
-  email: '',
-  password: '',
-  confirm_password: '',
+  email: "",
+  password: "",
+  confirm_password: "",
 };
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Email is required'),
+  email: Yup.string().email("Invalid email").required("Email is required"),
   password: Yup.string()
-    .min(6, 'Password must be at least 6 characters')
-    .required('Password is required'),
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
   confirm_password: Yup.string()
-    .oneOf([Yup.ref('password'), ''], 'Passwords must match')
-    .required('Confirmation is required'),
+    .oneOf([Yup.ref("password"), ""], "Passwords must match")
+    .required("Confirmation is required"),
 });
 
 const Register = () => {
@@ -43,15 +43,15 @@ const Register = () => {
 
     dispatch(registerUser(user)).then((res: any) => {
       console.log(res);
-      if (res.meta.requestStatus === 'fulfilled') {
-        const message = 'Signed up successfully😊';
-        const icon = 'success';
+      if (res.meta.requestStatus === "fulfilled") {
+        const message = "Signed up successfully😊";
+        const icon = "success";
         handleToastr(message, icon);
       }
-      if (res.meta.requestStatus === 'rejected') {
-        const message = 'Sign up unsuccessful🥺';
-        const text = res.payload ? res.payload.error_description[0] : '';
-        const icon = 'error';
+      if (res.meta.requestStatus === "rejected") {
+        const message = "Sign up unsuccessful🥺";
+        const text = res.payload ? res.payload.error_description[0] : "";
+        const icon = "error";
         handleToastr(message, icon, text);
       }
     });
@@ -59,7 +59,7 @@ const Register = () => {
 
   return (
     <section className={styles.register}>
-      <div className={styles['register-banner']}>
+      <div className={styles["register-banner"]}>
         <h1 className="text-2xl font-bold">
           Streamline your logistics operations more effectively with Trakade
         </h1>
@@ -72,9 +72,7 @@ const Register = () => {
         </div>
       </div>
 
-      <div className={styles['register-form']}>
-        {/* <h1>Register form</h1> */}
-
+      <div className={styles["register-form"]}>
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
@@ -170,19 +168,19 @@ const Register = () => {
 
         <div className="text-center text-sm">
           <p className="my-3">
-            By signing up, I agree to Trakade{' '}
+            By signing up, I agree to Trakade{" "}
             <Link to="/" className="underline">
               Terms of Service
-            </Link>{' '}
-            and{' '}
-            <Link to={'/'} className="underline">
+            </Link>{" "}
+            and{" "}
+            <Link to={"/"} className="underline">
               Privacy Policy
             </Link>
           </p>
           <p>
-            Already have an account?{' '}
+            Already have an account?{" "}
             <Link
-              to={'/login'}
+              to={"/login"}
               className="text-green-600 font-bold hover:underline transition-all duration-300 ease-in-out"
             >
               Login
