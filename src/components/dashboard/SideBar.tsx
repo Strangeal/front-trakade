d3b31c7b277aaae2afaaad742f7062f755a7f774
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-02 13:32:22 +0000

    Create the sidebar component

---
+++ src/components/dashboard/SideBar.tsx
@@ -0,0 +1,10 @@
+import styles from '../../styles/Home.module.css';
+
+const SideBar = () => {
+  return (
+    <section className={styles.sidebar}>
+      <h2 className="p-4">Sidebar</h2>
+    </section>
+  );
+};
+export default SideBar;
