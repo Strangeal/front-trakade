cad644e16820e3e1ec4eb4b2bd311e45594eaee3
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-02 13:32:38 +0000

    Create the top navigation component

---
+++ src/components/dashboard/TopNavigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../styles/Home.module.css';
+
+const TopNavigation = () => {
+  return (
+    <div className={styles['top-nav']}>
+      <h1>Top Navigation</h1>
+    </div>
+  );
+};
+
+export default TopNavigation;
