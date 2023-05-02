39d2171eae8bcba92bc3b283a9ae3da0ec121aad
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-02 13:31:42 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+// import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: { https: true },
+//   plugins: [mkcert()],
+// });
 export default defineConfig({
-  server: { https: true },
-  plugins: [mkcert()],
+  plugins: [react()],
 });
