c93e434072f5df1b1641861f92202c581f596aaa
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-02 13:33:19 +0000

    Update routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import "./index.css";
-import Register from "./pages/auth/Register";
-import Home from "./pages/Home";
-import store from "./redux/store";
-import Login from "./pages/auth/Login";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/dashboard/Home';
+import store from './redux/store';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
@@ -19,18 +19,18 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/register",
+        path: '/register',
         element: <Register />,
       },
       {
-        path: "/login",
+        path: '/login',
         element: <Login />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
