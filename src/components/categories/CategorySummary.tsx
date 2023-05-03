6788ab3305520d7894f5b72a58f3d84e0c36dd69
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-03 11:49:34 +0000

    Add component for category summary

---
+++ src/components/categories/CategorySummary.tsx
@@ -0,0 +1,39 @@
+import { IoIosArrowForward } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import styles from '../../styles/Categories.module.css';
+import CategoryCard from './CategoryCard';
+
+const CategorySummary = () => {
+  return (
+    <section className={styles.cat_container}>
+      <div>
+        <h1 className="font-bold text-2xl">Categories</h1>
+        <p className="leading-5 font-light">
+          View all your categories <br /> statistics here
+        </p>
+      </div>
+
+      <div className="flex gap-x-10">
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <CategoryCard />
+        <div className="bg-[#6ebcb7] text-white flex flex-col justify-between gap-y-4 p-4 w-[9rem] h-[10rem] rounded-3xl shadow-xl">
+          <h1 className="font-bold uppercase">
+            Full <br /> Stats
+          </h1>
+          <Link
+            to={'/dashboard/categories'}
+            className="bg-white p-3 rounded-full w-8 h-8 text-[#6ebcb7] flex justify-center items-center text-xl hover:shadow-sm hover:shadow-slate-800 transition-all duration-300 ease-in-out"
+          >
+            <span>
+              <IoIosArrowForward />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CategorySummary;
