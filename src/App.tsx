758cf479146e92f70942ae223c3c7dee556a9b3a
strangeal <bajerijustice@gmail.com>
2023-05-03 21:19:03 +0000

    Import ProSidebarProvider to be accessible across app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./components/Theme";
+import { ProSidebarProvider } from "react-pro-sidebar";
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <Outlet />
+        <ProSidebarProvider>
+          <Outlet />
+        </ProSidebarProvider>
       </ThemeProvider>
     </div>
   );
