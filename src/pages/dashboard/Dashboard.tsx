e62b1370794c2758be7dc9669c8228fd14579e51
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-08 13:23:14 +0000

    Add info on the number of users

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -73,6 +73,49 @@ const Dashboard = () => {
           </ul>
         </Card>
 
+        {/* Number of Users */}
+        <Card title={'No. of Users'}>
+          <div className="card-body flex justify-between">
+            <div className="bg-[#f8f8f8]  rounded-xl p-5 w-[9.5rem] h-[10.5rem]">
+              <div>
+                <svg
+                  viewBox="0 0 640 512"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={'75'}
+                >
+                  <path
+                    d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm32 32h-64c-17.6 0-33.5 7.1-45.1 18.6 40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64zm-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zm-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4z"
+                    fill="#38e079"
+                    className="fill-000000"
+                  ></path>
+                </svg>
+              </div>
+              <p className="text-sm text-gray-500 my-2">Total Customers</p>
+              <p className="font-extrabold text-xl">256</p>
+            </div>
+            <div className="bg-[#f8f8f8] rounded-xl p-5 w-[9.5rem] h-[10.5rem]">
+              <div>
+                <svg
+                  viewBox="0 0 16 16"
+                  width={'60'}
+                  // xml:space="preserve"
+                  xmlns="http://www.w3.org/2000/svg"
+                  enable-background="new 0 0 16 16"
+                >
+                  <path
+                    d="M7.516 11.094C7.688 10.609 9 8.938 9 6a3 3 0 1 0-6 0c0 2.938 1.313 4.609 1.484 5.094C2.953 11.234 0 12.211 0 14c0 .422.336 1 1 1h10c.664 0 1-.578 1-1 0-1.789-2.953-2.766-4.484-2.906zm5.132-3.055C12.889 7.311 14 5.273 14 4a3 3 0 0 0-5.143-2.098A5 5 0 0 1 11 6c0 1.521-.297 2.779-.645 3.748.775.307 1.527.723 2.146 1.252H15c.578 0 1-.453 1-1 0-1.258-2.422-1.789-3.352-1.961z"
+                    fill="#3392ff"
+                    className="fill-000000"
+                  ></path>
+                </svg>
+              </div>
+              <p className="text-sm text-gray-500 my-2">Total Suppliers</p>
+              <p className="font-extrabold text-xl">27</p>
+            </div>
+          </div>
+        </Card>
+      </section>
+
       <StockSummary />
       <SalesOrder />
     </>
