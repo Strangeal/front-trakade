3365c5113e9b465952b367a794d5e5aa8a24d4ae
strangeal <bajerijustice@gmail.com>
2023-05-08 13:36:41 +0000

    Add dummy data to populate sales

---
+++ src/helpers/dummyData.ts
@@ -0,0 +1,37 @@
+export const salesOverviews = [
+  {
+    title: "Total Sales",
+    num: 789,
+  },
+  {
+    title: "Revenue",
+    num: 789,
+  },
+  {
+    title: "Cost",
+    num: 789,
+  },
+  {
+    title: "Profit",
+    num: 789,
+  },
+];
+
+export const purchaseOverview = [
+  {
+    title: "No of Purchase",
+    num: 36,
+  },
+  {
+    title: "Canceled Order",
+    num: 30,
+  },
+  {
+    title: "Cost",
+    num: 498,
+  },
+  {
+    title: "Return",
+    num: 11,
+  },
+];
