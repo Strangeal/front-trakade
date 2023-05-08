0e5c239b772a72985570fb8771c22a70d1a746f9
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-05-08 14:48:25 +0000

    Add search bar to top navigation

--- src/components/dashboard/TopNavigation.tsx
+++ src/components/dashboard/TopNavigation.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { BiSearchAlt } from 'react-icons/bi';
+import { GrSettingsOption } from 'react-icons/gr';
+import { IoNotificationsSharp } from 'react-icons/io5';
+import { RxCaretDown } from 'react-icons/rx';
 import styles from '../../styles/Home.module.css';
 
 const TopNavigation = () => {
   return (
     <div className={styles['top-nav']}>
-      <h1>Top Navigation</h1>
+      <div>
+        <h1 className="font-bold text-xl">Good Morning Mafia👋</h1>
+        <p className="text-gray-500 text-sm font-bold">
+          Hope you have a nice day
+        </p>
+      </div>
+      <div className="flex items-center gap-x-7">
+        <div className="btn-group flex items-center gap-x-7 border-r-2 px-7">
+          <button
+            type="button"
+            title="search"
+            className="flex items-center gap-5 w-[24rem] shadow-lg hover:shadow-md transition-all duration-300 ease-in-out px-5 py-3 rounded-xl"
+          >
+            <span>
+              <BiSearchAlt className="text-gray-600" />
+            </span>
+            <p className="text-sm text-gray-600">
+              Search for a transaction, item, etc
+            </p>
+          </button>
     </div>
   );
 };
--- src/pages/dashboard/Home.tsx
+++ src/pages/dashboard/Home.tsx
@@ -1,16 +1,16 @@
-import { Outlet } from "react-router-dom";
-import { SideBar, TopNavigation } from "../../components/dashboard";
-import styles from "../../styles/Home.module.css";
+import { Outlet } from 'react-router-dom';
+import { SideBar, TopNavigation } from '../../components/dashboard';
+import styles from '../../styles/Home.module.css';
 
 const Home = () => {
   return (
     <section className={styles.home}>
       <SideBar />
 
-      <div className={styles["main-content"]}>
+      <div className={styles['main-content']}>
         <TopNavigation />
 
-        <div className="mt-4 mb-8">
+        <div className="p-4 mt-4 mb-8">
           <Outlet />
         </div>
       </div>
