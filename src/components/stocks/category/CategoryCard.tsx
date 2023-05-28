011f83354f85ae218d55eda6f475d4d4e9885cc5
strangeal <bajerijustice@gmail.com>
2023-05-28 11:08:28 +0000

    Define state

--- src/components/stocks/category/CategoryCard.tsx
+++ src/components/stocks/category/CategoryCard.tsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { BsTags } from "react-icons/bs";
 import { MdMoreVert } from "react-icons/md";
 import { TbEdit } from "react-icons/tb";
-import { Dialog } from "primereact/dialog";
-import CategoryDialog from "./CategoryDialog";
+import CategoryDialog from "../../dialog/AddCategoryDialog";
+import Categories from "./Categories";
+import ViewCategoryDialog from "../../dialog/ViewCategoryDialog";
 
 type CategoryProps = {
   list: {
@@ -21,15 +22,15 @@ const CategoryCard = ({ list }: CategoryProps) => {
     <div className="bg-white p-3 border rounded-lg hover:shadow-lg transition-all">
       <div className="grid place-items-center item-img bg-slate-600 overflow-hidden rounded-md relative">
         <img alt="Category image" src={list.image} className="w-100 h-56" />
-        <div>
-          <TbEdit className="absolute bottom-0 right-3 top-3 text-3xl text-[#14c8ab] bg-[#e8f5f4] p-1 rounded-lg flex hover:hidden" />
+        <div className="w-60">
+          <TbEdit className="absolute bottom-0 right-3 cursor-pointer top-3 text-3xl text-[#14c8ab] bg-[#e8f5f4] p-1 rounded-lg opacity-0 hover:opacity-100" />
         </div>
       </div>
       <div>
         <div className="flex items-center justify-between mt-3">
           <h4
             onClick={() => setVisible(true)}
-            className="font-bold text-slate-800 text-lg"
+            className="font-bold text-slate-800 text-lg cursor-pointer"
           >
             {list.name}
           </h4>
@@ -45,17 +46,8 @@ const CategoryCard = ({ list }: CategoryProps) => {
           <span className="">{list.category}</span>
         </p>
       </div>
-      <div className="card flex justify-content-center">
-        <Dialog
-          header="Header"
-          visible={visible}
-          style={{ width: "50vw" }}
-          onHide={() => setVisible(false)}
-          // footer={footerContent}
-        >
-          <CategoryDialog />
-        </Dialog>
-      </div>
+      {/* <CategoryDialog visible={visible} setVisible={setVisible} /> */}
+      <ViewCategoryDialog visible={visible} setVisible={setVisible} />
     </div>
   );
 };
